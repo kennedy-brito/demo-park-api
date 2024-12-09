c17bb4657ba7365575e9c5675a9d50c2433dd6d4
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-09 16:38:57 -0300

    feat: create Post user request

--- src/main/java/com/kennedy/demo_park_api/DemoParkApiApplication.java
+++ src/main/java/com/kennedy/demo_park_api/DemoParkApiApplication.java
@@ -3,6 +3,8 @@ package com.kennedy.demo_park_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoParkApiApplication {
 
---
+++ src/main/java/com/kennedy/demo_park_api/servicies/UserService.java
@@ -0,0 +1,22 @@
+package com.kennedy.demo_park_api.servicies;
+
+import com.kennedy.demo_park_api.entities.User;
+import com.kennedy.demo_park_api.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
