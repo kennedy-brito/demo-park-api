0ae4cd96b297784152e61ebda175c2b289079897
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-11 17:22:14 -0300

    chore(test): configure tests

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,5 @@
+insert into users (id, username, password, role)
+    values (100, 'ana@gmail.com', '123456', 'ROLE_ADMIN')
+    values (101, 'bia@gmail.com', '456789', 'ROLE_CLIENT')
+    values (102, 'bob@gmail.com', '123789', 'ROLE_CLIENT')
+
