ae3edaf2585b81186e55653a19bc66fcae3a6502
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-16 14:17:14 -0300

    documentation: document of create client request

--- src/main/java/com/kennedy/demo_park_api/web/controller/ClientController.java
+++ src/main/java/com/kennedy/demo_park_api/web/controller/ClientController.java
@@ -6,7 +6,14 @@ import com.kennedy.demo_park_api.servicies.ClientService;
 import com.kennedy.demo_park_api.servicies.UserService;
 import com.kennedy.demo_park_api.web.dto.ClientCreateDto;
 import com.kennedy.demo_park_api.web.dto.ClientResponseDto;
+import com.kennedy.demo_park_api.web.dto.UserResponseDto;
 import com.kennedy.demo_park_api.web.dto.mapper.ClientMapper;
+import com.kennedy.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +22,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Clients", description = "Contains all operations related to resources for creating, deleting, and editing users.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -23,6 +31,24 @@ public class ClientController {
     private final ClientService clientService;
     private final UserService userService;
 
+    @Operation(summary = "Create a new client",
+            description = "Resource for creating a new client. " + "Request restricted to authenticated user. Access restricted to Role='CLIENT'",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201", description = "resource created with success",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(
+                            responseCode = "403", description = "resource not accessible to ADMIN profile",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(
+                            responseCode = "409", description = "client cpf already in the system",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(
+                            responseCode = "422", description = "resource not processed because entry data was invalid",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
+
+            })
     @PostMapping
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<ClientResponseDto> create(
