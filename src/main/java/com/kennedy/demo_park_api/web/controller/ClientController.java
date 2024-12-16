6fa6b8ca12ce81e481e271a73c0c441179ff1708
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-16 17:02:55 -0300

    feat: list all clients new default values

--- src/main/java/com/kennedy/demo_park_api/web/controller/ClientController.java
+++ src/main/java/com/kennedy/demo_park_api/web/controller/ClientController.java
@@ -13,6 +13,9 @@ import com.kennedy.demo_park_api.web.dto.mapper.ClientMapper;
 import com.kennedy.demo_park_api.web.dto.mapper.PageableMapper;
 import com.kennedy.demo_park_api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -22,6 +25,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -39,6 +43,7 @@ public class ClientController {
 
     @Operation(summary = "Create a new client",
             description = "Resource for creating a new client. " + "Request restricted to authenticated user. Access restricted to Role='CLIENT'",
+            security = @SecurityRequirement(name = "security"),
             responses = {
                     @ApiResponse(
                             responseCode = "201", description = "resource created with success",
@@ -97,7 +102,7 @@ public class ClientController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping()
-    public ResponseEntity<PageableDto> findAll(Pageable pageable){
+    public ResponseEntity<PageableDto> findAll(@Parameter(hidden = true) @PageableDefault(size = 5, sort = {"name"}) Pageable pageable){
         Page<ClientProjection> clients = clientService.findAll(pageable);
 
         return ResponseEntity.ok(
