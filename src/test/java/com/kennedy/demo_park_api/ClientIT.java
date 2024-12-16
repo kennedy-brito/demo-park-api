b7e03e5c13ce292f56bef1c726b3c8bad4f76a10
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-16 15:03:51 -0300

    test: test create Client (status: 403, 409, 422)

--- src/test/java/com/kennedy/demo_park_api/ClientIT.java
+++ src/test/java/com/kennedy/demo_park_api/ClientIT.java
@@ -2,10 +2,12 @@ package com.kennedy.demo_park_api;
 
 import com.kennedy.demo_park_api.web.dto.ClientCreateDto;
 import com.kennedy.demo_park_api.web.dto.ClientResponseDto;
+import com.kennedy.demo_park_api.web.exception.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -36,5 +38,86 @@ public class ClientIT {
         Assertions.assertThat(responseBody.getId()).isNotNull();
         Assertions.assertThat(responseBody.getName()).isEqualTo("Tobias Ferreira");
         Assertions.assertThat(responseBody.getCpf()).isEqualTo("91809057060");
+    }
+
+    @Test
+    public void createClient_withRegisteredCpf_ReturnErrorMessageWithStatus409(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri(base_url + "/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,"toby@gmail.com", "123456"))
+                .bodyValue(new ClientCreateDto("Tobias Ferreira", "02268984079"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.CONFLICT)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.CONFLICT.value());
+    }
+
+    @Test
+    public void createClient_withInvalidData_ReturnErrorMessageWithStatus422(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri(base_url + "/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,"toby@gmail.com", "123456"))
+                .bodyValue(new ClientCreateDto("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+        responseBody = testClient
+                .post()
+                .uri(base_url + "/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,"toby@gmail.com", "123456"))
+                .bodyValue(new ClientCreateDto("toby", "01234567891"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+        responseBody = testClient
+                .post()
+                .uri(base_url + "/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,"toby@gmail.com", "123456"))
+                .bodyValue(new ClientCreateDto("toby", "918.090.570-60"))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+    }
+
+    @Test
+    public void createClient_withUserWithoutPermission_ReturnErrorMessageWithStatus403(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri(base_url + "/clients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient,"ana@gmail.com", "123456"))
+                .bodyValue(new ClientCreateDto("Tobias Ferreira", "91809057060"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(HttpStatus.FORBIDDEN.value());
+
     }
 }
