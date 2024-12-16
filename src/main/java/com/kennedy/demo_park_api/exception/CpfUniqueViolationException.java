552a1cf8acbfe36244f994ea1e6a806a71f8057d
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-16 13:54:26 -0300

    feat: create Client unique cpf violation exception

---
+++ src/main/java/com/kennedy/demo_park_api/exception/CpfUniqueViolationException.java
@@ -0,0 +1,8 @@
+package com.kennedy.demo_park_api.exception;
+
+public class CpfUniqueViolationException extends RuntimeException {
+
+    public CpfUniqueViolationException(String message) {
+        super(message);
+    }
+}
