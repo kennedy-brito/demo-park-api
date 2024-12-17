132fbdb1da0de2dd28baaf9bd83ade8880aad618
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-17 15:18:29 -0300

    feat: create Parking util, and method to generate receipt

---
+++ src/main/java/com/kennedy/demo_park_api/util/ParkingUtils.java
@@ -0,0 +1,20 @@
+package com.kennedy.demo_park_api.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ParkingUtils {
+
+    public static String generateReceipt(){
+        LocalDateTime date = LocalDateTime.now();
+        String receipt = date.toString().substring(0,19);
+
+        return receipt
+                .replace("-", "")
+                .replace(":", "")
+                .replace("T","-");
+    }
+}
