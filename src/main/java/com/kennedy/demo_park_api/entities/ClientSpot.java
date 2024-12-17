0f4dbd20602b3e47829df18c52b6c9e18f34fcac
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-17 15:03:59 -0300

    feat: create ClientSpot class

---
+++ src/main/java/com/kennedy/demo_park_api/entities/ClientSpot.java
@@ -0,0 +1,94 @@
+package com.kennedy.demo_park_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_client_has_spot")
+@EntityListeners(AuditingEntityListener.class)
+public class ClientSpot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "receipt_number", nullable = false, unique = true, length = 15)
+    private String receipt;
+
+    @Column(name = "plate", nullable = false, length = 8)
+    private String plate;
+
+    @Column(name = "brand", nullable = false, length = 45)
+    private String brand;
+
+    @Column(name = "model", nullable = false, length = 45)
+    private String model;
+
+    @Column(name = "color", nullable = false, length = 45)
+    private String color;
+
+    @Column(name = "entry_date", nullable = false)
+    private LocalDateTime entryDate;
+
+    @Column(name = "exit_date")
+    private LocalDateTime exitDate;
+
+    @Column(name = "value", columnDefinition = "DECIMAL(7,2)")
+    private BigDecimal value;
+
+    @Column(name = "discount", columnDefinition = "DECIMAL(7,2)")
+    private BigDecimal discount;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "id_spot", nullable = false)
+    private Spot spot;
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ClientSpot that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
