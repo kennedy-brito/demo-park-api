771ba04f6f90c4d49d3a53518f1c75e6edd7f2d8
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-18 15:04:58 -0300

    feat: method to calculate price

--- src/main/java/com/kennedy/demo_park_api/util/ParkingUtils.java
+++ src/main/java/com/kennedy/demo_park_api/util/ParkingUtils.java
@@ -3,10 +3,17 @@ package com.kennedy.demo_park_api.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ParkingUtils {
+    
+    private static final double FIRST_15_MINUTES = 5.00;
+    private static final double FIRST_60_MINUTES = 9.25;
+    private static final double ADITIONAL_15_MINUTES = 1.75;
 
     public static String generateReceipt(){
         LocalDateTime date = LocalDateTime.now();
@@ -16,5 +23,34 @@ public class ParkingUtils {
                 .replace("-", "")
                 .replace(":", "")
                 .replace("T","-");
+    }
+
+
+
+    public static BigDecimal calculatePrice(LocalDateTime entrada, LocalDateTime saida) {
+        long minutes = entrada.until(saida, ChronoUnit.MINUTES);
+        double total = 0.0;
+
+        if (minutes <= 15) {
+
+            total += FIRST_15_MINUTES;
+
+        } else if (minutes <= 60) {
+
+            total += FIRST_60_MINUTES;
+
+        } else {
+
+            total += FIRST_60_MINUTES;
+            minutes -= 60;
+
+            while (minutes > 0){
+                total += ADITIONAL_15_MINUTES;
+                minutes -= 15;
+            }
+
+        }
+
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
     }
 }
